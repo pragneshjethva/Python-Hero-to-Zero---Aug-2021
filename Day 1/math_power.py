c97823b7079fe846c0acbf14caefe2ac669e063d
pragneshjethva <89073658+pragneshjethva@users.noreply.github.com>
2021-08-17 15:26:02 +0530

    Python Hero to Zero - Day 1 Assignment Submission

---
+++ Day 1/math_power.py
@@ -0,0 +1,16 @@
+#Assignment - Take two inputs and store in variable x and y, find x to the power y.
+x = int(input("Enter Base number: "))
+y = int(input("Enter Index number: "))
+
+power = x ** y
+print("{} to the power {} is: {}".format(x,y,power))
+
+"""
+#Python program to find power of any number using power function
+
+x = int(input("Enter the Base : "))
+y = int(input("Enter the index : "))
+power = pow(x, y)
+print("Answer : ",power)
+
+"""
